d4bcb3b008c93af014589b91d7f8186aa3e5ffbf
Sunnyspp <sunnyspp2013@gmail.com>
2023-01-02 07:46:22 -0800

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
 """
 
 
@@ -20,19 +19,16 @@ if __name__ == "__main__":
         print("N must be at least 4")
         exit(1)
 
-    # initialize the answer list
     for i in range(n):
         a.append([i, None])
 
     def already_exists(y):
-        """check that a queen does not already exist in that y value"""
         for x in range(n):
             if y == a[x][1]:
                 return True
         return False
 
     def reject(x, y):
-        """determines whether or not to reject the solution"""
         if (already_exists(y)):
             return False
         i = 0
@@ -43,20 +39,17 @@ if __name__ == "__main__":
         return True
 
     def clear_a(x):
-        """clears the answers from the point of failure on"""
         for i in range(x, n):
             a[i][1] = None
 
     def nqueens(x):
-        """recursive backtracking function to find the solution"""
         for y in range(n):
             clear_a(x)
             if reject(x, y):
                 a[x][1] = y
-                if (x == n - 1):  # accepts the solution
+                if (x == n - 1):
                     print(a)
                 else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+                    nqueens(x + 1)
 
-    # start the recursive process at x = 0
     nqueens(0)
