2c02badb706b33fac80232f416a74af8dbbf5b49
Sunnyspp <sunnyspp2013@gmail.com>
2023-03-13 14:50:31 -0700

    updated

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-const args = argv.slice(2);
-let result = 0;
-let finalArray = [];
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
 
-if (args.length > 1) {
-  finalArray = [...new Set(args.map((e) => parseInt(e)).sort((a, b) => b - a))];
-  result = finalArray.length > 1 ? finalArray[1] : finalArray[0];
-}
+myObject.value = 89;
 
-console.log(result);
+console.log(myObject);
